3c35c7a7eb66a4d8d010afd75a988762eb28c375
Monica Grages <mgrages1@gmail.com>
2017-11-18 16:10:07 -0500

    set up page navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,24 @@ def index
   render "home"
 end
 
+def mission
+	render "mission"
+end
+
+def inspections
+	render "inspections"
+end
+
+def underwriting
+	render "underwriting"
+end
+
+def solutions
+	render "solutions"
+end
+
+def contact
+	render "contact"
+end
+
 end
